78395528ce6d5b2b957d04c1c917a1eb79784e44
danishrashidin <danishrashidin@gmail.com>
2022-01-03 17:41:14 +0800

    Changes by Ajwad

--- src/Garden/Garden.java
+++ src/Garden/Garden.java
@@ -10,4 +10,24 @@ public class Garden {
     public Garden(SeasonFactory seasonFactory) {
         this.seasonFactory = seasonFactory;
     }
+
+    public void showTree(){
+        System.out.println(this.seasonFactory.createTree().showTree());
+    }
+
+    public void showGround(){
+        System.out.println(this.seasonFactory.createGround().showGround());
+    }
+
+    public void setAnimal(Animal animal) {
+        this.animal = animal;
+    }
+
+    public void setAnimalMovingBehavior(MoveBehaviour moveBehaviour){
+        animal.setMoveBehaviour(moveBehaviour);
+    }
+
+    public void decorateAnimal(AnimalsDecorator animalsDecorator){
+
+    }
 }
